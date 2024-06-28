3a6af19ab148d6a4770a2faa704f69c27ac5ec6e
Eric Martin <eric.martin@hey.com>
2024-06-28 12:49:49 +0200

    feat: add spacing with homepage headings

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default async function HomePage() {
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <main className="container flex flex-col p-8">
-        <h1 className="font-bold text-4xl">
+        <h1 className="font-bold text-4xl mb-8">
           Find all about your favorite characters !
         </h1>
-        <h2 className="font-semibold text-2xl">Check out these two...</h2>
+        <h2 className="font-semibold text-2xl mb-4">Check out these two...</h2>
         <h2 className="font-semibold text-2xl">
           ...or search for a specific one !
         </h2>
