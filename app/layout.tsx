a8777f394aed08bf490803e4bd879c2383d88d36
Eric Martin <eric.martin@hey.com>
2024-06-28 14:14:10 +0200

    feat: logo redirects to homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
             <nav className="container gap-6 text-lg font-medium flex flex-row items-center md:gap-5 md:text-sm lg:gap-6">
               <Link
-                href="#"
+                href="/"
                 className="flex items-center gap-2 text-lg font-semibold md:text-base"
               >
                 <BookHeart className="h-6 w-6" />
