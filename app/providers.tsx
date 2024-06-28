628fcb881de495cedd9bb449cda0fdd2da2ff961
Eric Martin <eric.martin@hey.com>
2024-06-28 14:19:14 +0200

    build: remove typescript errors on build

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@ import { getQueryClient } from "@/app/get-query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export default function Providers({ children }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   // NOTE: Avoid useState when initializing the query client if you don't
   //       have a suspense boundary between this and the code that may
   //       suspend because React will throw away the client on the initial
